c2b4acf2158194c69ad4952d9f5e57ac35de0098
Madar-B <daniilkorneev00940@gmail.com>
2023-02-10 17:44:33 +0200

    tht.2.3

--- Tehtävä 2.3.py
+++ Tehtävä 2.3.py
@@ -5,7 +5,5 @@ y = "Korkeus"
 x = 4
 y = 6
 print("Mitkä ovat kanta ja korkeus?")
-print("Pinta-ala on")
-print(x * y)
-print("Piiri on")
-print(x+x+y+y)
+print("Pinta-ala on", (x*y))
+print("Piiri on", (x+x+y+y))
